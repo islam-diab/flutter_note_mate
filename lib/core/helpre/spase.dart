9e487bd854b4d75d4d8a476e2b73f31193230a12
Islam Diab <islamdiab304@gmail.com>
2024-07-08 23:06:53 +0300

    - Add assets fill and file string helper - Add spase file helper

---
+++ lib/core/helpre/spase.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+SizedBox horizontalSpace(double weight) => SizedBox(
+      width: weight.w,
+    );
+SizedBox verticalSpace(double height) => SizedBox(
+      height: height.h,
+    );
---
+++ lib/note_mate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NoteMate extends StatelessWidget {
+  const NoteMate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: null,
+    );
+  }
+}
